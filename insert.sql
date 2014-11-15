490ca341490467f885b919a5d47f4c77029ac1f2
Cooper Johnson <lavosprime@gmail.com>
2014-11-15 00:32:37 -0800

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,6 @@
+-- Copyright (c) 2014 by Cooper Johnson <lavosprime@gmail.com>
+-- This program is free software provided under the terms of the MIT License.
+--
+-- insert.sql: Query to insert a single tuple into the database.
+
+INSERT INTO Plays VALUES('%1', '%2');
